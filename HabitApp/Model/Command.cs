fd28bf56fd03fe2577550dd1dcea9b32568a70ce
Artem Zhikharev <artemziharew@gmail.com>
2022-05-23 22:48:30 +0300

    Исправлены базовые классы команд
    Тестовая команда закрытия приложения добавлена в окно регистрации, её работа протестирована

--- HabitApp/Model/Command.cs
+++ HabitApp/Model/Command.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HabitApp.Model
 {
-    public interface ICommand
+    public abstract class Command : ICommand
     {
-        bool CanExecute(object parameter);
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
 
-        void Execute(object parameter);
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
--- HabitApp/VM/LoginVM.cs
+++ HabitApp/VM/LoginVM.cs
@@ -1,36 +1,74 @@
-﻿using HabitApp.Data;
-using HabitApp.Model;
+﻿using HabitApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HabitApp.VM
 {
     public class LoginVM : ViewModel
     {
-        private User currentUser = null;
 
-        private string _username;
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Habit App";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Username : string - Имя пользователя
+
+        /// <summary>Имя пользователя</summary>
+        private string _Username;
+
+        /// <summary>Имя пользователя</summary>
         public string Username
         {
-            get { return _username; }
-            set { _username = value; OnPropertyChanged(); }
+            get => _Username;
+            set => Set(ref _Username, value);
         }
 
-        private string _password;
+        #endregion
+
+        #region Password : string - Пароль
+
+        /// <summary>Пароль</summary>
+        private string _Password;
+
+        /// <summary>Пароль</summary>
         public string Password
         {
-            get { return _password; }
-            set { _password = value; OnPropertyChanged(); }
+            get => _Password;
+            set => Set(ref _Password, value);
         }
 
-        public ICommand Login { get; }
-        private void OnLoginExecuted(object p)
+        #endregion
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
         {
-            // тут ещё подумать
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        public LoginVM()
+        {
+            CloseApplicationCommand = new BaseCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
         }
     }
 }
