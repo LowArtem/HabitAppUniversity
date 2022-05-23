31d91140bd5c9938d82820f48c43f21e54a206d5
Artem Zhikharev <artemziharew@gmail.com>
2022-05-23 20:13:23 +0300

    Созданы первые рабочие классы структуры
    - Создан базовый класс ViewModel и его примерная реализация для окна регистрации
    - Создан базовый класс комманд
    - Создан класс данных User с соответствующими полями и базы данных

---
+++ HabitApp/Data/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitApp.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public long Experience { get; set; }
+        public long Money { get; set; }
+        public int? GroupId { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(int id, string username, string password, long experience, long money, int? groupId)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Experience = experience;
+            Money = money;
+            GroupId = groupId;
+        }
+    }
+}
---
+++ HabitApp/VM/LoginVM.cs
@@ -0,0 +1,36 @@
+﻿using HabitApp.Data;
+using HabitApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitApp.VM
+{
+    public class LoginVM : ViewModel
+    {
+        private User currentUser = null;
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public ICommand Login { get; }
+        private void OnLoginExecuted(object p)
+        {
+            // тут ещё подумать
+        }
+    }
+}
