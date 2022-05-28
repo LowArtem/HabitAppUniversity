7a269e3c1d9e78cef653f383e0d1edd2856899ba
Artem Zhikharev <artemziharew@gmail.com>
2022-05-29 00:18:21 +0300

    Добавлен класс для категорий задач и типов привычек
    Хранит в себе различные возможные категории (типы). Пользователь может только выбрать одну из предложенных
    
    Также обновлено состояние .csproj файла, который не был добавлен в предыдущие коммиты, хотя его состояние изменялось

---
+++ HabitApp/Data/Categories.cs
@@ -0,0 +1,12 @@
+﻿namespace HabitApp.Data
+{
+    public struct Categories
+    {
+        public const string Health = "Health";
+        public const string Education = "Education";
+        public const string Entertainment = "Entertainment";
+        public const string SelfDevelopment = "Self-Development";
+        public const string Household = "Household";
+        public const string Work = "Work";
+    }
+}
---
+++ HabitApp/Data/Type.cs
@@ -0,0 +1,9 @@
+﻿namespace HabitApp.Data
+{
+    public struct Type
+    {
+        public const int Positive = 1;
+        public const int Negative = 2;
+        public const int Universal = 3;
+    }
+}
