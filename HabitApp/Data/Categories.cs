1925878b9833496596b43d06df273c23443d3e57
Artem Zhikharev <artemziharew@gmail.com>
2022-05-29 15:54:21 +0300

    Добавлены методы в класс Category, добавлены Extensions
    В Extensions добавлен конвертер приходящего из БД типа привычки (int) в bool переменную в зависимости от номера переключателя (чтобы выбрать нужный тип)
    
    Аналогичный конвертер добавлен в класс категорий, чтобы выбирать в комбобоксе нужную категорию в зависимости от приходящей строки

--- HabitApp/Data/Categories.cs
+++ HabitApp/Data/Categories.cs
@@ -1,6 +1,10 @@
-﻿namespace HabitApp.Data
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HabitApp.Data
 {
-    public struct Categories
+    public static class Categories
     {
         public const string Health = "Health";
         public const string Education = "Education";
@@ -8,5 +12,53 @@
         public const string SelfDevelopment = "Self-Development";
         public const string Household = "Household";
         public const string Work = "Work";
+        public const string Other = "Other";
+
+        public static List<string> GetAll()
+        {
+            var list = new List<string>();
+            list.Add(Health);
+            list.Add(Education);
+            list.Add(Entertainment);
+            list.Add(SelfDevelopment);
+            list.Add(Household);
+            list.Add(Work);
+            list.Add(Other);
+
+            return list;
+        }
+    }
+
+    public class CategoryConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString())
+            {
+                case Categories.Health: return 0;
+                case Categories.Education: return 1;
+                case Categories.Entertainment: return 2;
+                case Categories.SelfDevelopment: return 3;
+                case Categories.Household: return 4;
+                case Categories.Work: return 5;
+                case Categories.Other: return 6;
+                default: return 0;
+            }
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (int.Parse(value.ToString()))
+            {
+                case 0: return Categories.Health;
+                case 1: return Categories.Education;
+                case 2: return Categories.Entertainment;
+                case 3: return Categories.SelfDevelopment;
+                case 4: return Categories.Household;
+                case 5: return Categories.Work;
+                case 6: return Categories.Other;
+                default: return 0;
+            }
+        }
     }
 }
--- HabitApp/Model/Extensions.cs
+++ HabitApp/Model/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace HabitApp.Model
 {
@@ -10,6 +12,31 @@ namespace HabitApp.Model
             int i;
             if (int.TryParse(s, out i)) return i;
             return null;
+        }
+    }
+
+    public class IntTypeToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int valueInt)
+            {
+                if (parameter.ToString() == "1" && valueInt == 1) return true;
+                else if (parameter.ToString() == "2" && valueInt == 2) return true;
+                else if (parameter.ToString() == "3" && valueInt == 3) return true;
+                else return false;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return int.Parse(parameter.ToString());
+            }
+            return 1;
         }
     }
 }
