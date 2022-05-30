b27aa94d960a71cfc4cad483fcd49a78f76e699e
Artem Zhikharev <artemziharew@gmail.com>
2022-05-30 09:48:55 +0300

    Исправлено обновление листбоксов
    Костыльное исправление, но работает, через code-behind подписываюсь на событие обновления списка и вручную заставляю листбокс перерисовывать коллекцию
    
    Более правильный вариант, видимо, реализовывать INotifyPropertyChanged на классе элемента списка

--- HabitApp/View/HomeView.xaml.cs
+++ HabitApp/View/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using HabitApp.VM;
+using System.Windows.Controls;
 
 namespace HabitApp.View
 {
@@ -10,6 +11,26 @@ namespace HabitApp.View
         public HomeView()
         {
             InitializeComponent();
+
+            if (this.DataContext != null)
+            {
+                ((HomeVM)this.DataContext).PropertyChanged += HomeVM_PropertyChanged;
+            }
+        }
+        private void HomeVM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(HomeVM.Habits))
+            {
+                this.HabitListBox.Items.Refresh();
+            }
+            else if (e.PropertyName == nameof(HomeVM.DailyHabits))
+            {
+                this.DailyHabitListBox.Items.Refresh();
+            }
+            else if (e.PropertyName == nameof(HomeVM.Tasks))
+            {
+                this.TaskListBox.Items.Refresh();
+            }
         }
     }
 }
