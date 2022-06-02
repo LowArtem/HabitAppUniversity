863d905ec2628caf3169a4d8083b7997f84181e2
Artem Zhikharev <artemziharew@gmail.com>
2022-06-02 21:37:47 +0300

    Очистка лишних директив using

--- HabitApp/Implementation/StatisticsRepository.cs
+++ HabitApp/Implementation/StatisticsRepository.cs
@@ -1,11 +1,6 @@
-﻿using HabitApp.Model;
-using Npgsql;
+﻿using Npgsql;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HabitApp.Implementation
 {
